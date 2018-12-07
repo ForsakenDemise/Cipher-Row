bd3dc00ea7ec0956051d8f0cf15464735e9e145e
macktommyZ <45474870+macktommyZ@users.noreply.github.com>
2018-12-06 16:53:52 -0800

    Update project_case_1.h

--- project_case_1.h
+++ project_case_1.h
@@ -1,73 +1,100 @@
 // Project Case 1: Inland Empire Solar Sales Travel
-
 #include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
 
-// Function Prototype
-int returnCity(string s);
-void calDistance();
-void calBreadth();
-void calShortest();
+// Function Prototypes
+int returnCity(string s); // Return integer value of the city using String param var.
+void calDistance();       // Calculate the distance between two cities.
+void calBreadth();        // Calculate the Breadth First Search of cities.
+void calShortest();       // Determine the Shortest path for the saleman to travel.
+void calLowest();         // Calculate the Lowest Cost Trip
 
-// Cities
-const int RIVERSIDE = 0;
+// Cities: Each city is represented by a constant integer value.
+const int RIVERSIDE     = 0;
 const int MORENO_VALLEY = 1;
-const int PERRIS = 2;
-const int HEMET = 3; 
+const int PERRIS        = 2;
+const int HEMET         = 3; 
 
-// Menu Selection
-const int SELECT_DISTANCE = 1;
-const int SELECT_BREADTH = 2;
-const int SELECT_SHORTEST = 3;
 
-// Multidimension-Array to represent the weighted graph of cities
-int graph[4][4] =  {{0, 22, 26, 33},
-                    {22, 0, 20, 29},
-                    {26, 20, 0, 32},
-                    {33, 29, 32, 0}};
+// Multidimension-Array to represent the matrix of cities.
+const int graph[4][4] =  {{0, 22, 26, 33},
+                          {22, 0, 20, 29},
+                          {26, 20, 0, 32},
+                          {33, 29, 32, 0}};
 
-int cities[4];
 
-// Main Function
+// Main Function: The Program will begin here. 
 int main()
 {
-  // Print the Menu Selection Screen.
-  cout << "*************************************"   << endl;
-  cout << "* Inland Empire Solar Sales Travel  *"   << endl;
-  cout << "*************************************"   << endl << endl;
-  cout << "Please select your option:\n" << endl;
-  cout << "1. Calculate distance between cities" << endl;
-  cout << "2. Calculate Breadth First Search" << endl;
-  cout << "3. Calculate Shortest Path" << endl << endl;
-  
-  // Prompt user for an input for Menu Selection.
-  cout << "Selection: ";
-  int userInput;
-  cin >> userInput;
-  
-  // Consume the white space from the keyboard buffer.
-  cin.ignore();   
+  bool exitProgram = false;
 
-  // Start the function based on the user-input.
-  if (userInput == SELECT_DISTANCE)
-    calDistance();
-  
-  else if (userInput == SELECT_BREADTH)
-    calBreadth();
-  
-  else if (userInput == SELECT_SHORTEST)
-    calShortest();
+  // Continue running the program until the user choose "Exit Application".
+  while (!exitProgram)
+  {
+    // Print Menu Selection.
+    cout << " ___       _                 _   _____                 _"                << endl;           
+    cout << "|_ _|_ __ | | __ _ _ __   __| | | ____|_ __ ___  _ __ (_)_ __ ___"       << endl;  
+    cout << " | || '_ \\| |/ _` | '_ \\ / _` | |  _| | '_ ` _ \\| '_ \\| | '__/ _ \\" << endl;  
+    cout << " | || | | | | (_| | | | | (_| | | |___| | | | | | |_) | | | |  __/ "     << endl; 
+    cout << "|___|_| |_|_|\\__,_|_| |_|\\__,_| |_____|_| |_| |_| .__/|_|_|  \\___| "  << endl; 
+    cout << "                                                |_|                "     << endl;
+    cout << "*************************************"             << endl;
+    cout << "* Inland Empire Solar Sales Travel  *"             << endl;
+    cout << "*************************************"     << endl << endl;
+    cout << "Please select your option:"                << endl << endl;
+    cout << "1. Calculate Distance between two cities"          << endl;
+    cout << "2. Calculate Breadth First Search"                 << endl;
+    cout << "3. Calculate Shortest Path"                        << endl;
+    cout << "4. Calculate the lowest cost trips"        << endl << endl;
+    cout << "5. Exit Application."                      << endl << endl;
+
+
+    // Menu Selection: Each selection is represented by an integer value.
+    const int SELECT_DISTANCE = 1;
+    const int SELECT_BREADTH  = 2;
+    const int SELECT_SHORTEST = 3;
+    const int SELECT_LOWEST = 4;
+    const int SELECT_EXIT = 5;
+    
+    // Prompt user for an integer input for the Menu Selection.
+    cout << "Selection: ";
+    int userInput;
+    cin >> userInput;
+    cin.ignore();     // Consume the white space from the keyboard buffer.
+
+    // Start the function based on the user-input.
+    if (userInput == SELECT_DISTANCE)
+      calDistance();
+    
+    else if (userInput == SELECT_BREADTH)
+      calBreadth();
+    
+    else if (userInput == SELECT_SHORTEST)
+      calShortest();
+
+    else if (userInput == SELECT_LOWEST)
+      calLowest();
+
+    else if (userInput == SELECT_EXIT)
+    {
+      exitProgram = true;
+      cout << "Program has successfully closed.";
+    }
+
+    // Create empty lines for the next menu selection appearance.
+    for (auto i=0;i<10;i++)
+      cout << "\n";
+  }
 
   return 0;
 }
 
-// This function will compare the user input to determine their city in integer.
+// The function will compare the user input to determine their city in integer.
+// This function requires a String city name and will return its integer.
 int returnCity(string userString)
 {
-
   if (userString.compare("riverside") == 0)
     return RIVERSIDE;
 
@@ -83,89 +110,43 @@ int returnCity(string userString)
   return 0;
 }
 
-// This function will calculate the distance between the two input cities
+// This function will calculate the distance between the two input cities.
+// This feature is not a requirement for the Case 1 Project, but it is helpful
+// to validate if our matrix adjancency in the program is portraying accurate value.
 void calDistance()
 {
   string userInput;
   int startCity;  // The city that the user will start from.
   int endCity;    // The city that the user will exit to.
 
-  cout << "\nEnter the starting city: ";
+  cout << "\n\n[ 1. Calculate Distance between two cities ]\n";
+
+  cout << "\nEnter the Starting city: ";
   getline(cin, userInput);
   startCity = returnCity(userInput);
-  
 
-  cout << "Enter the ending city: ";
+  cout << "Enter the Ending city: ";
   getline(cin, userInput);
   endCity = returnCity(userInput);
 
-  cout << "Distance: " << graph[startCity][endCity] << endl;
+  cout << "\nDistance: " << graph[startCity][endCity] << " miles." << "\t <--- Your Answer\n";
 }
 
 // This function will calculate the Breadth First Search
 void calBreadth()
 {
-
+  cout << "\n\n[ 2. Calculate Breadth First Search ]\n";
 }
 
 // This function will calculate the Shortest Path. 
 void calShortest()
 {
-  string userInput;
-  const int startCity = RIVERSIDE; // The default Starting City is Riverside;
-  int endCity;                     // The city that the user will exit to.
-
-  // This is a dynamic available pool of cities that can be used in the program.
-  // For example, if the user select a city, remove that city from the pool.
-  // The pool can later be used to generate a list of possible combination of city path.
-  vector<int> cityPool = {RIVERSIDE, MORENO_VALLEY, PERRIS, HEMET};
-
-  // Prompt for the ending city for the user.
-  cout << "Enter the ending city: ";
-  getline(cin, userInput);
-  endCity = returnCity(userInput);
-
-  // Remove the startCity and endCity from the cityPool Vector.
-  cityPool.erase(cityPool.begin(), startCity + 2);
-  cityPool.erase(endCity);
-
-  // Create a city-to-travel vector for the shortest path.
-  // Algorithm will use a vector to allow the swap of middle-cities; not start or end.
-  vector<int> cityList;
-
-  // Insert the Starting City to the Front of Vector
-  cityList.push_back(startCity);
-
-  // Insert the Middle-Cities
-  cityList.push_back(cityPool.front());
-  cityList.push_back(cityPool.last());
-  
-  // Insert the Ending City last.
-  cityList.push_back(endCity);
-
-  // Prompt for Ending City input
+  cout << "\n\n[ 3. Calculate Shortest Path ]\n";
 }
 
-// A class for representing the city to be used in our case project.
-// This class contains the value of its name and constant number; ex. Riverside = 1
-class City 
+// This function will calculate the Lowest Cost Trips
+void calLowest()
 {
-  private:
-  string name;
-  int num;
-
-  public:
-  void setName(string n)
-  {
-    name = n;
-  }
-
-  string getName()
-  {
-    return name;
-  }
-
-};
-
-
+  cout << "\n\n[ 4. Calculate the lowest cost trips ]\n";
+}
 
